bd94d3d6d1bc3f78511f67a5d231d606d45e875b
Ajay KC <meajay64@gmail.com>
2022-09-12 23:57:21 +0900

    Commit completed design

---
+++ index.js
@@ -0,0 +1,31 @@
+let userRating;
+let ratingsPara = document.querySelectorAll(".ratings p");
+let submitButton = document.querySelector("button");
+let ratingsParaArr = Array.from(ratingsPara);
+
+ratingsParaArr.forEach((item) => {
+  item.addEventListener("click", () => {
+    userRating = Number(item.textContent);
+
+    ratingsParaArr.forEach((item) => {
+      item.classList.remove("clicked");
+    });
+
+    item.classList.add("clicked");
+  });
+});
+
+submitButton.addEventListener("click", () => {
+  if (!userRating) {
+    alert("Please give rating first before submitting.");
+    return;
+  }
+  let mainContainer = document.querySelector(".container");
+  let thankYouContainer = document.querySelector(".container__thankyou");
+  let ratingSpan = document.querySelector(".js-rating");
+
+  ratingSpan.textContent = userRating;
+
+  mainContainer.classList.add("hide");
+  thankYouContainer.classList.remove("hide");
+});
